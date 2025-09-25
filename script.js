cdfe134a492c1398d4126ac888c3ee8d26f67c8e
Natte.py <nathaniel.engstrom@elev.ga.ntig.se>
2025-09-25 19:59:19 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 let clicks = 0;
 let clickers = false;
+let clickeramount = 0;
 
-document.getElementById("btn").addEventListener("click", function() {
+
+let clickersprice = 10;
+
+function buyclcks(){
+    if (clicks >= clickersprice && clickers === true){
+        document.getElementById("buyclickers").disabled = false
+    }
+    else{
+        document.getElementById("buyclickers").disabled = true
+    }
+    document.getElementById("buyclickers").addEventListener("click", function(){
+        clickeramount++
+        clicks = clicks - clickersprice
+    })
+}
+
+
+
+
+document.getElementById("btn").addEventListener("click", function(){
     clicks++;
+    console.log(clicks);
     document.getElementById("num").innerText = clicks;
+
+
+    if (clicks >= 10 && clickers === false){
+    document.getElementById("buyclickers").disabled = false
+}
+    
+    // if (clicks >= 10 && clickers === true ){
+        
+    // }
+
 });
+
